302d83b73c5ce5040e28ef95d1fd6f125e2cd373
Tal Zadik <talzadik@Tals-MacBook-Pro.local>
2022-06-19 21:06:54 +0300

    Daily exercises-day1-Tal Zadik

--- D01/EX02/index.js
+++ D01/EX02/index.js
@@ -1,5 +1,11 @@
-function isBiggerThan() {
-  //your code
+function isBiggerThan(num1) {
+  if (num1>4) {
+    console.log (`The number ${num1} is bigger than 4`);
+  } else if (num1===4) {
+    console.log (`The number ${num1} is equal to 4`);
+  } else {
+  console.log (`The number ${num1} is less than 4`);
+  }
 }
 
 // Do not remove or change this line, or the tests won't work
--- D01/EX06/index.js
+++ D01/EX06/index.js
@@ -1,3 +1,19 @@
 function moduloEx6(number, comparer) {
-  //your code
+  let result = number % comparer;
+  let sentence = "";
+
+  if (number > comparer) {
+    sentence = `The number ${number} is bigger than ${comparer}`;
+    if (result !== 0) {
+      sentence += `. But the modulo of ${number} % ${comprare} is ${result}`;
+    }
+  } else if (number === comparer) {
+    sentence = `The number ${number} is equal to ${comparer}`;
+  } else {
+    sentence = `The number ${number} is less than ${comparer}`;
+  }
+  return sentence;
 }
+
+document.getElementById("output").innerText=moduloEx6(5, 10);
+console.log('hello');
